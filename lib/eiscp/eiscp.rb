501d8739ab86a04d68dc7c00e187f99d0987978a
Michael Rodrigues <mikebrodrigues@gmail.com>
2013-12-29 18:31:28 -0800

    drying up eiscp methods

--- lib/eiscp/eiscp.rb
+++ lib/eiscp/eiscp.rb
@@ -10,41 +10,8 @@ class EISCP
     @host = host
   end
 
-  def self.discover
-    sock = UDPSocket.new
-    sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
-    sock.send(ONKYO_MAGIC, 0, '<broadcast>', ONKYO_PORT)
-    data = []
-    while true
-      ready = IO.select([sock], nil, nil, 0.5)
-      if ready != nil
-        then readable = ready[0]
-      else
-        return data
-      end
 
-
-      readable.each do |socket|
-        begin
-          if socket == sock
-            msg, addr = sock.recvfrom_nonblock(1024)
-            data << [msg, addr[2]]
-          end
-        rescue IO::WaitReadable
-          retry
-        end
-      end
-
-    end
-  end
-
-  def send(eiscp_packet)
-    sock = TCPSocket.new @host, ONKYO_PORT
-    sock.puts eiscp_packet
-    sock.close
-  end
-
-  def recv(sock, timeout = nil)
+  def self.recv(sock, timeout = nil)
     data = []
     while true
       ready = IO.select([sock], nil, nil, timeout)
@@ -65,51 +32,34 @@ class EISCP
         end
       end
 
-
-
     end
+  end
 
+  def self.discover
+    sock = UDPSocket.new
+    sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+    sock.send(ONKYO_MAGIC, 0, '<broadcast>', ONKYO_PORT)
+    self.recv(sock, 0.5)
+  end
+
+  def send(eiscp_packet)
+    sock = TCPSocket.new @host, ONKYO_PORT
+    sock.puts eiscp_packet
+    sock.close
   end
 
   def send_recv(eiscp_packet)
     sock = TCPSocket.new @host, ONKYO_PORT
     sock.puts eiscp_packet
-    puts recv(sock, 1)
+    self.recv(sock, 0.5)
   end
 
 
   def connect
     sock = TCPSocket.new @host, ONKYO_PORT
-    buffer = ""
-    while true
-      ready = IO.select([sock], nil, nil, nil)
-      if ready != nil
-        then readable = ready[0]
-      else
-        break
-      end
-
-      readable.each do |socket|
-        begin
-          if socket == sock
-            buffer += sock.recv_nonblock(1024).chomp
-          end
-        rescue IO::WaitReadable
-          retry
-        end
-      end
-
-      messages = buffer.split("\r\n")
-      unless messages.count > 1
-        continue
-      else
-        puts messages.shift
-        buffer = messages[0]
-      end
-
-      
-    end
+    self.recv(sock, nil)
   end
+
 
 
 end
