0164181e1a54c1051820cae1f3de7eaaf094ea19
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-01-02 02:41:27 -0800

    changes to onkyo server

---
+++ bin/onkyo-server.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require 'eiscp/eiscp_server'
+
+puts "Starting server on 60128..."
+
+EISCPServer.new
--- lib/eiscp/eiscp_server.rb
+++ lib/eiscp/eiscp_server.rb
@@ -7,6 +7,8 @@ class EISCPServer
 
   ONKYO_DISCOVERY_RESPONSE =  EISCPPacket.new("ECN", "TX-NR609/60128/DX/001122334455")
 
+  # Create/start the server object.
+
   def initialize
     Socket.udp_server_loop("255.255.255.255", 60128) do |msg, msg_src|
       msg_src.reply ONKYO_DISCOVERY_RESPONSE
