25a5cddf1f56859ca2c94b184a516a2b71e91d11
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-01-02 01:06:29 -0800

    command tests

---
+++ test/tc_command.rb
@@ -0,0 +1,30 @@
+require_relative "../lib/eiscp/command.rb"
+require "test/unit"
+
+class TestCommand < Test::Unit::TestCase
+
+  def test_command_to_name
+    assert_equal(Command.command_to_name("PWR"), "system-power")
+  end
+
+  def test_name_to_command
+    assert_equal(Command.name_to_command("system-power"), "PWR")
+  end
+
+  def test_command_value_to_value_name
+    assert_equal(Command.command_value_to_value_name("PWR", "01"), "on")
+  end
+
+  def test_command_value_name_to_value
+    assert_equal(Command.command_value_name_to_value("PWR", "on"), "01")
+  end
+
+  def test_description_from_name
+    assert_equal(Command.description_from_name("system-power"), "System Power Command")
+  end
+
+  def test_description_from_command
+    assert_equal(Command.description_from_command("PWR"), "System Power Command")
+  end
+
+end
