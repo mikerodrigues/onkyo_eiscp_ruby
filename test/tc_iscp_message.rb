2e8a266dd132bb91538613ecc47b3937fb43f970
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-01-05 22:05:59 -0800

    cleaned up tests for iscp_message

--- test/tc_iscp_message.rb
+++ test/tc_iscp_message.rb
@@ -3,12 +3,25 @@ require "test/unit"
 
 class TestISCPMessage <  Test::Unit::TestCase
 
+  DISCOVERY_STRING = "ISCP\x00\x00\x00\x10\x00\x00\x00\n\x01\x00\x00\x00!xECNQSTN"
+  DISCOVERY_PACKET = EISCPPacket.new('ECN', 'QSTN', 'x', '!')
+
+
+
   def test_create_discovery_iscp_message
-    assert_equal(ISCPMessage.new("ECN", "QSTN", "x", "!").to_s, "!xECNQSTN\r")
+    assert_equal(ISCPMessage.new("ECN", "QSTN", "x", "!").to_iscp, "!xECNQSTN")
   end
 
   def test_parse_discovery_iscp_message
-    assert_equal(ISCPMessage.parse("!xECNQSTN\r").to_s, "!xECNQSTN\r")
+    assert_equal(ISCPMessage.parse("!xECNQSTN").to_iscp, "!xECNQSTN")
+  end
+
+  def test_create_discovery_packet_string
+    assert_equal(DISCOVERY_PACKET.to_eiscp, DISCOVERY_STRING)
+  end
+
+  def test_parse_discovery_packet_string
+    assert_equal(ISCPMessage.parse(DISCOVERY_STRING).to_eiscp, DISCOVERY_PACKET.to_eiscp)
   end
 
 end
