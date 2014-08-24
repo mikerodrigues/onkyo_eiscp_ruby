84aea7e49d41ea228b8855c3a2d5d6a42bd88739
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-08-24 01:58:35 -0700

    refactoring Receiver

--- lib/eiscp/receiver.rb
+++ lib/eiscp/receiver.rb
@@ -1,7 +1,9 @@
 require 'socket'
-require_relative './message'
-require_relative './dictionary'
 require 'resolv'
+require_relative './message'
+require_relative './receiver'
+require_relative'./receiver/discovery'
+require_relative'./receiver/connection'
 
 module EISCP
   # The EISCP::Receiver class is used to communicate with one or more
@@ -13,6 +15,10 @@ module EISCP
   #   receiver = EISCP::Receiver.new('192.168.1.12', 60129) # non standard port
   #
   class Receiver
+
+    extend Discovery
+    include Connection
+
     # Receiver's IP address
     attr_accessor :host
     # Receiver's model string
@@ -23,15 +29,6 @@ module EISCP
     attr_accessor :area
     # Receiver's MAC address
     attr_accessor :mac_address
-
-    # Receiver's connection socket
-    attr_reader :socket
-    # Receiver's connection thread
-    attr_reader :thread
-    # Receiver's message response queue
-    attr_reader :queue
-    # Most recent message received
-    attr_reader :last
 
     # ISCP Magic Packet for Autodiscovery
     ONKYO_MAGIC = Message.new(command: 'ECN', value: 'QSTN', terminator: "\r\n", unit_type: 'x').to_eiscp
@@ -50,7 +47,11 @@ module EISCP
         @port  = hash[:port]
         @area  = hash[:area]
         @mac_address = hash[:mac_address]
-        @socket = TCPSocket.new(@host, @port)
+        if block_given?
+          connect(@host, @port, block)
+        else
+          connect(@host, @port)
+        end
       end
 
       # This lambda sets the host IP after resolving it
@@ -83,116 +84,8 @@ module EISCP
         set_attrs.call info_hash
       end
 
-      # This handles the background thread for monitoring messages from the
-      # receiver.
-      #
-      # If a block is given, it can be used to setup a callback when a message
-      # is received.
-      #
-      @queue = Queue.new
-      @thread = Thread.new do
-        while true
-          @last = recv
-          if @queue.size >= 10
-            @queue.shift
-          end
-          if block_given?
-            block.call(msg)
-          end
-        end
-      end
     end
 
-    # Populates attrs with info from ECNQSTN response
-    #
-    def self.ecn_string_to_ecn_array(ecn_string)
-      hash = {}
-      message = EISCP::Message.parse(ecn_string)
-      array = message.value.split('/')
-      hash[:model] = array.shift
-      hash[:port] = array.shift.to_i
-      hash[:area] = array.shift
-      hash[:mac_address] = array.shift
-      hash
-    end
-
-    # Returns an array of arrays consisting of a discovery response packet
-    # string and the source ip address of the reciever.
-    #
-    def self.discover(discovery_port = ONKYO_PORT)
-      sock = UDPSocket.new
-      sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
-      sock.send(ONKYO_MAGIC, 0, '<broadcast>', discovery_port)
-      data = []
-      loop do
-
-        begin
-          msg, addr = sock.recvfrom_nonblock(1024)
-          data << Receiver.new(addr[2], ecn_string_to_ecn_array(msg))
-        rescue IO::WaitReadable
-          io = IO.select([sock], nil, nil, 0.5)
-          if io.nil?
-            return data
-          else
-            retry
-          end
-        end
-
-      end
-    end
-
-    # Sends an EISCP::Message object or string on the network
-    #
-    def send(eiscp, timeout = 0.5)
-      if eiscp.is_a? EISCP::Message
-        @socket.puts(eiscp.to_eiscp)
-      elsif eiscp.is_a? String
-        if Message.parse eiscp
-          @socket.puts eiscp
-        else
-          fail
-        end
-      end
-    end
-
-    def recv(timeout = 0.5)
-      mesg_str = ''
-      until mesg_str.match(/\r\n$/)
-        mesg_str << @socket.gets
-      end
-      Message.parse mesg_str
-    end
-
-    # Sends an EISCP::Message object or string on the network and returns
-    # recieved data string.
-    #
-    def send_recv(eiscp, timeout = 0.5)
-      if eiscp.is_a? EISCP::Message
-        @socket.puts(eiscp.to_eiscp)
-      elsif eiscp.is_a? String
-        if Message.parse eiscp
-          @socket.puts(eiscp)
-        else
-          fail
-        end
-      end
-      sleep 0.1
-      @last
-    end
-
-    # Open a TCP connection to the host and print all received messages until
-    # killed.
-    #
-    def connect(&block)
-      loop do
-        data = recv
-        if block_given?
-          yield data
-        else
-          puts data.to_s
-        end
-      end
-    end
 
     # Return ECN array with model, port, area, and MAC address
     #
@@ -211,7 +104,7 @@ module EISCP
       command_name = sym.to_s.gsub(/_/, '-')
       value_name = args[0].to_s.gsub(/_/, '-')
       begin
-        send_recv EISCP::Message.parse(command_name + ' ' + value_name)
+        @connection.send_recv Message.parse(command_name + ' ' + value_name)
       rescue
         puts "Could not find a command: #{command_name} with args #{value_name} and block #{block}"
       end
