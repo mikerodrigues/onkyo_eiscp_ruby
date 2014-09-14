29de2dd9037504efddaccafe775c8ea073d8c614
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-09-14 06:26:44 -0700

    slight updates to dictionary_helpers doc

--- lib/eiscp/dictionary/dictionary_helpers.rb
+++ lib/eiscp/dictionary/dictionary_helpers.rb
@@ -46,7 +46,7 @@ module EISCP
         end
       end
 
-      # Return a command value name from a command and value
+      # Return a value name from a command and a value
       def command_value_to_value_name(command, value)
         begin
           zone = zone_from_command(command)
@@ -56,7 +56,7 @@ module EISCP
         end
       end
 
-      # Return a command value from a command and value name
+      # Return a value from a command and value name
       def command_value_name_to_value(command, value_name)
         begin
           zone = zone_from_command(command)
@@ -68,7 +68,7 @@ module EISCP
         end
       end
 
-      # Return a description form a command name and zone
+      # Return a command description from a command name and zone
       def description_from_command_name(name, zone)
         @commands[zone].each_pair do |command, attrs|
           if attrs[:name] == name
@@ -78,13 +78,13 @@ module EISCP
         nil
       end
 
-      # Return a description from a command
+      # Return a command description from a command
       def description_from_command(command)
         zone = zone_from_command(command)
         @commands[zone][command][:description]
       end
 
-      # Return a description from a command and value
+      # Return a value description from a command and value
       def description_from_command_value(command, value)
         zone = zone_from_command(command)
         @commands[zone][command][:values].select do |k, v|
@@ -102,6 +102,8 @@ module EISCP
         sets
       end
 
+      # Checks to see if the command is in the Dictionary
+      #
       def known_command?(command)
         begin
           zone = zone_from_command(command)
